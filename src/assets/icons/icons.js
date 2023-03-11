f45b47a62fda67fc4ab50ee4264ffa26a2cc22a3
Manav Jasani <manavjasani12@gmail.com>
2023-03-12 01:21:51 +0530

    Add, update, delete formdata done

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -67,3 +67,38 @@ export const saveIcon = (
     <polyline points="7 3 7 8 15 8"></polyline>
   </svg>
 );
+
+export const addIcon = (
+  <svg
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    strokeWidth="2"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="css-i6dzq1"
+  >
+    <circle cx="12" cy="12" r="10"></circle>
+    <line x1="12" y1="8" x2="12" y2="16"></line>
+    <line x1="8" y1="12" x2="16" y2="12"></line>
+  </svg>
+);
+
+export const closeIcon = (
+  <svg
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    stroke-width="2"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="css-i6dzq1"
+  >
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+  </svg>
+);
--- src/components/Device.js
+++ src/components/Device.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Form, Table } from "react-bootstrap";
 import {
+  addIcon,
+  closeIcon,
   DeleteIcon,
   DownArrow,
   EditIcon,
@@ -8,18 +10,36 @@ import {
 } from "../assets/icons/icons";
 
 const Device = () => {
-  const [id, setId] = useState(1);
+  const [id, setId] = useState("");
+  const [editableData, setEditableData] = useState("");
+
   const [deviceName, setDeviceName] = useState("");
   const [editableId, setEditableId] = useState("");
-  const [miles, setMiles] = useState(1);
-  const [menu, setMenu] = useState("");
+  const [menu, setMenu] = useState("PC");
   const [deviceEnabled, setDeviceEnabled] = useState(false);
+  const [addData, setAddData] = useState(false);
 
   const [formDataArray, setFormDataArray] = useState(
-    JSON.parse(localStorage.getItem("formData"))
+    JSON.parse(localStorage.getItem("formData")) || []
   );
 
+  console.log("editableData", editableData);
+  //   const [miles, setMiles] = useState(formDataArray.length - 1);
+
   useEffect(() => {
+    if (editableData) {
+      setDeviceName(editableData?.deviceName);
+      setMenu(editableData?.menu);
+      setDeviceEnabled(editableData?.deviceEnabled);
+    }
+  }, [editableData]);
+
+  useEffect(() => {
+    setEditableData("");
+    setEditableId("");
+    setDeviceName("");
+    setMenu("PC");
+    setDeviceEnabled(false);
     localStorage.setItem("formData", JSON.stringify(formDataArray));
   }, [formDataArray]);
 
@@ -29,21 +49,74 @@ const Device = () => {
     setDeviceName(e.target.value);
   };
 
-  const saveDataHandler = () => {};
+  const addFormDataHandler = () => {
+    setAddData(true);
+    // setFormDataArray([
+    //   ...formDataArray,
+    //   {
+    //     id: formDataArray.length + 1,
+    //     deviceName: "",
+    //     miles: "",
+    //     menu: "",
+    //     deviceEnabled: false,
+    //   },
+    // ]);
+  };
+  const removeFormDataHandler = () => {
+    setAddData(false);
+    // setFormDataArray([
+    //   ...formDataArray,
+    //   {
+    //     id: formDataArray.length + 1,
+    //     deviceName: "",
+    //     miles: "",
+    //     menu: "",
+    //     deviceEnabled: false,
+    //   },
+    // ]);
+  };
+  console.log("addData", addData);
+
+  const deleteDataHandler = (id) => {
+    const data = formDataArray.filter((item, index) => item.id !== id);
+    setFormDataArray(data);
+    console.log("data", data);
+  };
+
+  const saveDataHandler = (data) => {
+    setEditableId(data.id);
+    setEditableData(data);
+  };
+
+  console.log("EditableId", editableId);
 
   const createDataHandler = () => {
     const data = {
-      id,
+      id: editableData ? editableData.id : formDataArray.length + 1,
       deviceName,
-      miles,
+      miles: editableData
+        ? editableData.miles
+        : formDataArray.length > 0
+        ? formDataArray[formDataArray.length - 1].miles + 1
+        : 1,
       menu,
       deviceEnabled,
     };
     console.log("data", data);
-    setFormDataArray([...formDataArray, data]);
+    if (editableId) {
+      const editableArr = formDataArray.map((item) => {
+        if (editableData.id === item.id) {
+          return data;
+        }
+        return item;
+      });
+      setFormDataArray(editableArr);
+    } else {
+      setFormDataArray([...formDataArray, data]);
+    }
   };
 
-  console.log("menu", deviceName, menu, deviceEnabled);
+  console.log("menu", formDataArray);
 
   return (
     <div>
@@ -59,21 +132,25 @@ const Device = () => {
             <th>Type</th>
             <th>Enabled</th>
             <th>Actions</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {formDataArray?.map((item, index) => {
             return (
-              <tr key={item.index}>
+              <tr key={index}>
                 <td>
                   <Form.Control
                     type="text"
                     placeholder="Enter device name"
-                    value={item.deviceName}
+                    value={
+                      editableId === item.id ? deviceName : item.deviceName
+                    }
                     onChange={deviceNameHandler}
+                    disabled={editableId !== item.id}
                   />
                 </td>
-                <td>{`${miles - 1} - ${miles}`} Miles</td>
+                <td>{`${item.miles - 1} - ${item.miles}`} Miles</td>
                 <td>
                   <Form.Select
                     aria-label="Default select example"
@@ -81,7 +158,8 @@ const Device = () => {
                       console.log("e.target.value", e.target.value);
                       setMenu(e.target.value);
                     }}
-                    defaultValue={item.menu}
+                    defaultValue={editableId === item.id ? menu : item.menu}
+                    disabled={editableId !== item.id}
                   >
                     {/* <option>Open this select menu</option> */}
                     <option value="PC">PC</option>
@@ -92,22 +170,93 @@ const Device = () => {
                   <Form.Check
                     onChange={(e) => setDeviceEnabled(e.target.checked)}
                     type="checkbox"
-                    checked={item.deviceEnabled}
+                    checked={
+                      editableId === item.id
+                        ? deviceEnabled
+                        : item.deviceEnabled
+                    }
                     id={`device-enabled`}
+                    disabled={editableId !== item.id}
                   />
                 </td>
                 <td>
                   <div>
-                    <button onClick={() => saveDataHandler()}>
-                      {EditIcon}
+                    {editableId !== item.id && (
+                      <button onClick={() => saveDataHandler(item)}>
+                        {EditIcon}
+                      </button>
+                    )}
+                    {editableId === item.id && (
+                      <button onClick={createDataHandler}>{saveIcon}</button>
+                    )}
+                    <button onClick={() => deleteDataHandler(item.id)}>
+                      {DeleteIcon}
                     </button>
-                    <button onClick={createDataHandler}>{saveIcon}</button>
-                    <button>{DeleteIcon}</button>
                   </div>
+                </td>
+                <td>
+                  {index === formDataArray.length - 1 && !addData && (
+                    <button onClick={addFormDataHandler}>{addIcon}</button>
+                  )}
                 </td>
               </tr>
             );
           })}
+          {(addData || formDataArray.length === 0) && (
+            <tr>
+              <td>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter device name"
+                  value={!editableId ? deviceName : ""}
+                  onChange={deviceNameHandler}
+                  disabled={editableId}
+                />
+              </td>
+              <td>
+                {`${
+                  formDataArray.length > 0
+                    ? formDataArray[formDataArray.length - 1]?.miles +
+                      " - " +
+                      (formDataArray[formDataArray.length - 1]?.miles + 1)
+                    : "0 - 1 "
+                }`}{" "}
+                Miles
+              </td>
+              <td>
+                <Form.Select
+                  aria-label="Default select example"
+                  onChange={(e) => {
+                    setMenu(e.target.value);
+                  }}
+                  defaultValue={menu}
+                  disabled={editableId}
+                >
+                  <option value="PC">PC</option>
+                  <option value="Tab">Tab</option>
+                </Form.Select>
+              </td>
+              <td>
+                <Form.Check
+                  onChange={(e) => setDeviceEnabled(e.target.checked)}
+                  type="checkbox"
+                  checked={!editableId ? deviceEnabled : false}
+                  id={`device-enabled`}
+                  disabled={editableId}
+                />
+              </td>
+              <td>
+                <div>
+                  <button onClick={createDataHandler}>{saveIcon}</button>
+                </div>
+              </td>
+              <td>
+                {addData && (
+                  <button onClick={removeFormDataHandler}>{closeIcon}</button>
+                )}
+              </td>
+            </tr>
+          )}
         </tbody>
       </Table>
     </div>
@@ -115,3 +264,55 @@ const Device = () => {
 };
 
 export default Device;
+
+{
+  /* <tr>
+  <td>
+    <Form.Control
+      type="text"
+      placeholder="Enter device name"
+      value={!editableId ? deviceName : ""}
+      onChange={deviceNameHandler}
+      disabled={editableId}
+    />
+  </td>
+  <td>
+    {`${
+      formDataArray.length > 0
+        ? formDataArray[formDataArray.length - 1]?.miles +
+          " - " +
+          (formDataArray[formDataArray.length - 1]?.miles + 1)
+        : "0 - 1 "
+    }`}{" "}
+    Miles
+  </td>
+  <td>
+    <Form.Select
+      aria-label="Default select example"
+      onChange={(e) => {
+        setMenu(e.target.value);
+      }}
+      defaultValue={menu}
+      disabled={editableId}
+    >
+      <option value="PC">PC</option>
+      <option value="Tab">Tab</option>
+    </Form.Select>
+  </td>
+  <td>
+    <Form.Check
+      onChange={(e) => setDeviceEnabled(e.target.checked)}
+      type="checkbox"
+      checked={!editableId ? deviceEnabled : false}
+      id={`device-enabled`}
+      disabled={editableId}
+    />
+  </td>
+  <td>
+    <div>
+      <button onClick={createDataHandler}>{saveIcon}</button>
+
+    </div>
+  </td>
+</tr>; */
+}
